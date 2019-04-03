6d3289c018b2539005179b1fa76e2a3776564d28
Stephen Maitland <s.maitland@me.com>
2019-04-03 15:22:56 +0100

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Stephen on 03/04/2019.
+//  Copyright © 2019 Stephen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
